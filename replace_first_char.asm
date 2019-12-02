b31bb1de293d1807efb11e9dc0ebc7c260aca428
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-02 02:42:12 +0100

    replace

---
+++ replace_first_char.asm
@@ -0,0 +1,33 @@
+; int func(char *a) 
+; convery first characters of words in string to a 'w'
+
+section .text
+global func
+
+func: 
+  push ebp
+  mov  ebp, esp
+  mov  eax, DWORD [ebp+8] ; address of *a to eax
+  mov  BYTE [eax], 'w'
+  mov  ecx, 0              ; count = 0
+
+loop: 
+  mov  bl, [eax]
+  cmp  bl, 0               ; if cmp is true, we are at end of string
+  je  loop_exit
+  inc exc                  ; count++
+  cmp bl, 'Z'
+  jle modify
+  inc eax                  ; a++
+  jmp loop
+
+modify: 
+  mov BYTE [eac], '*'
+  inc eax
+  jmp loop
+
+loop_exit: 
+  mov eax, ecx
+  pop ebp
+  ret
+
