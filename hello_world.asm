fa314f3047dddd9c3c05044169e8d31577128e84
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-02 02:16:45 +0100

    update x86

--- hello_world.asm
+++ hello_world.asm
@@ -1,10 +1,9 @@
-section     .data
+  section     .data
 msg     db  'Hello, world!', 10    ;our dear string
 len     equ $-msg    ;length of our dear string
-section     .text
 
-global      _start   ;must be declared for linker (ld)
-
+  section     .text
+  global      _start   ;must be declared for linker (ld)
 _start:               ;tell linker entry point
 
     mov     edx, len   ;message length
@@ -13,7 +12,7 @@ _start:               ;tell linker entry point
     mov     eax, 4     ;system call number (sys_write)
     int 0x80
 
-    mov     eax, 60      ;system call number (sys_exit)
-    xor edi, edi ; exit code = 0
+    mov eax, 1      ;system call number (sys_exit)
+    xor ebx, ebx ; exit code = 0
     int 0x80
 
